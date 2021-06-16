b801ed16e636cfa0a8df8832ca60f3660c5e7887
SIG UReport <ureport@sig.bj>
2021-06-16 17:32:56 +0100

    init project

---
+++ reveri_games/test/reveri_games_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:reveri_games/reveri_games.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+  });
+}
