10aaba07badce5ecec5b0fbbbca53ffb1edbcf54
SIG UReport <ureport@sig.bj>
2021-06-21 07:47:10 +0100

    online test

--- reveri_games/digital_project_manager/lib/manual_map/game_manual_map.dart
+++ reveri_games/digital_project_manager/lib/manual_map/game_manual_map.dart
@@ -17,59 +17,70 @@ class GameManualMap extends StatelessWidget implements GameListener {
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     SystemChrome.setEnabledSystemUIOverlays([]);
     return LayoutBuilder(builder: (context, constraints) {
       DungeonMap.tileSize =
           max(constraints.maxHeight, constraints.maxWidth) / (kIsWeb ? 20 : 22);
-      return (platformType.isWeb && MediaQuery.of(context).orientation == Orientation.portrait) ? Center(
-          child: TextButton(
-            onPressed: () async {
-              await SystemChrome.setPreferredOrientations([
-                DeviceOrientation.landscapeLeft,
-                DeviceOrientation.landscapeRight,
-              ]);
-            },
-            child: Text('Changez l\'orientation'),
-          ))
-          :
-      BonfireWidget(
-        joystick: Joystick(
-          keyboardEnable: true,
-          directional: JoystickDirectional(
-            spriteBackgroundDirectional: Sprite.load('joystick_background.png'),
-            spriteKnobDirectional: Sprite.load('joystick_knob.png'),
-            size: 100,
-            isFixed: false,
-          ),
-          actions: [
-            JoystickAction(
-              actionId: PlayerAttackType.AttackMelee,
-              sprite: Sprite.load('joystick_atack.png'),
-              align: JoystickActionAlign.BOTTOM_RIGHT,
-              size: 80,
-              margin: EdgeInsets.only(bottom: 50, right: 50),
+      return
+      Stack(
+        children: [
+          BonfireWidget(
+            joystick: Joystick(
+              keyboardEnable: true,
+              directional: JoystickDirectional(
+                spriteBackgroundDirectional: Sprite.load('joystick_background.png'),
+                spriteKnobDirectional: Sprite.load('joystick_knob.png'),
+                size: 100,
+                isFixed: false,
+              ),
+              actions: [
+                JoystickAction(
+                  actionId: PlayerAttackType.AttackMelee,
+                  sprite: Sprite.load('joystick_atack.png'),
+                  align: JoystickActionAlign.BOTTOM_RIGHT,
+                  size: 80,
+                  margin: EdgeInsets.only(bottom: 50, right: 50),
+                ),
+                JoystickAction(
+                  actionId: PlayerAttackType.AttackRange,
+                  sprite: Sprite.load('joystick_atack_range.png'),
+                  spriteBackgroundDirection: Sprite.load('joystick_background.png'),
+                  size: 50,
+                  enableDirection: true,
+                  margin: EdgeInsets.only(bottom: 50, right: 160),
+                )
+              ],
             ),
-            JoystickAction(
-              actionId: PlayerAttackType.AttackRange,
-              sprite: Sprite.load('joystick_atack_range.png'),
-              spriteBackgroundDirection: Sprite.load('joystick_background.png'),
-              size: 50,
-              enableDirection: true,
-              margin: EdgeInsets.only(bottom: 50, right: 160),
+            player: Knight(
+              Vector2((4 * DungeonMap.tileSize), (6 * DungeonMap.tileSize)),
+            ),
+            interface: KnightInterface(),
+            map: DungeonMap.map(),
+            enemies: DungeonMap.enemies(),
+            decorations: DungeonMap.decorations(),
+            background: BackgroundColorGame(Colors.blueGrey[900]!),
+            components: [],
+            gameController: _controller..setListener(this),
+            lightingColorGame: Colors.black.withOpacity(0.75),
+          ),
+          if (platformType!.isWeb && MediaQuery.of(context).orientation ==
+              Orientation.portrait)
+            Scaffold(
+              body: Center(child: TextButton(
+                  onPressed: () async {
+                    await SystemChrome.setPreferredOrientations([
+                      DeviceOrientation.landscapeLeft,
+                      DeviceOrientation.landscapeRight,
+                    ]);
+                  },
+                  child: Text('Changez l\'orientation', style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w600,
+                  ),),
+                )),
             )
-          ],
-        ),
-        player: Knight(
-          Vector2((4 * DungeonMap.tileSize), (6 * DungeonMap.tileSize)),
-        ),
-        interface: KnightInterface(),
-        map: DungeonMap.map(),
-        enemies: DungeonMap.enemies(),
-        decorations: DungeonMap.decorations(),
-        background: BackgroundColorGame(Colors.blueGrey[900]!),
-        components: [],
-        gameController: _controller..setListener(this),
-        lightingColorGame: Colors.black.withOpacity(0.75),
+        ],
       );
     });
   }
