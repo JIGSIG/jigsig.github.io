5d2f9ad5754a7c603cbc7ee62f075e10fd37a058
SIG UReport <ureport@sig.bj>
2021-06-21 14:41:57 +0100

    online test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "3b87efbef95234194a2fbf9662bee323",
-"index.html": "754a24f6ab08005c8a654e247b74c0bf",
-"/": "754a24f6ab08005c8a654e247b74c0bf",
-"main.dart.js": "6fa3c11eb37fcbf833982e1fad24ea3e",
+"index.html": "1a1dc952b4393fc90836736fd3a608db",
+"/": "1a1dc952b4393fc90836736fd3a608db",
+"main.dart.js": "ab2d5babbcd67670e3038f06360c39d3",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
