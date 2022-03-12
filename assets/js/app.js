757a9eba7ac2a8d68216573522e14adbb2f59385
Sean DAHISSIHO <john.dahissiho@gmail.com>
2022-03-12 10:45:05 +0100

    portfolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,13 +11,14 @@ class FlutterApp extends HTMLElement {
           width: ${width};
           margin: 0 auto;
           border: none;
+          position: absolute;
+          left: 85%;
+          top: 50%;
+          -webkit-transform: translate(-85%, -50%);
+          transform: translate(-85%, -50%);      
         }
       </style>
-    <iframe 
-        id="app-container"
-        src="app.html">
-    </iframe>
-        `;
+      <iframe id="app-container" src="https://www.youtube.com/embed/jATox3OCefw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>        `;
     }
 }
 
