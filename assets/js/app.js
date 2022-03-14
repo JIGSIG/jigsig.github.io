fcb28d5a608411b0a94f24803192f378a895f0e7
Sean DAHISSIHO <john.dahissiho@gmail.com>
2022-03-14 07:45:28 +0100

    portfolio

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+
+class FlutterApp extends HTMLElement {
+  constructor() {
+    super();
+    const width = this.getAttribute("width") || "100%";
+    const height = this.getAttribute("height") || "100%";
+    this.innerHTML = `
+    <style>
+        #app-container {
+          height: ${height};
+          width: ${width};
+          margin: 0 auto;
+          border: none;
+        }
+      </style>
+      <iframe id="app-container" src="https://jigsig.github.io"></iframe>        `;
+  }
+}
+
+customElements.define('flutter-app', FlutterApp);
